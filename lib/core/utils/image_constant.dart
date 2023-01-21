f3e7703602b713d8239dd2e82ea64bac6a6b7464
Dewanshi Dewan <Dewanshi.Dewan@iiitb.ac.in>
2023-01-21 17:11:05 +0530

    Delete lib directory

--- lib/core/utils/image_constant.dart
+++
@@ -1,7 +0,0 @@
-class ImageConstant {
-  static String imgLayer1 = 'assets/images/img_layer1.svg';
-
-  static String imgGroup60 = 'assets/images/img_group60.svg';
-
-  static String imageNotFound = 'assets/images/image_not_found.png';
-}
